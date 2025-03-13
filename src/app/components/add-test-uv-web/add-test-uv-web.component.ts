261f35b2e42f565c4938f7d4140f0fcfd6cffbec
yodoeaoffi06 <yadhirtv@gmail.com>
2025-03-13 08:18:20 -0600

    validations in form added

--- src/app/components/add-test-uv-web/add-test-uv-web.component.ts
+++ src/app/components/add-test-uv-web/add-test-uv-web.component.ts
@@ -5,15 +5,33 @@ import { TestUvWebService } from 'src/app/services/test-uv-web.service';
 @Component({
   selector: 'app-add-test-uv-web',
   templateUrl: './add-test-uv-web.component.html',
-  styleUrls: ['./add-test-uv-web.component.css']
+  styleUrls: ['./add-test-uv-web.component.css'],
 })
 export class AddTestUvWebComponent {
   test: Test = new Test();
   submitted = false;
 
-  constructor(private testUvWebService: TestUvWebService) { }
+  constructor(private testUvWebService: TestUvWebService) {}
 
   saveTest(): void {
+    if (
+      !this.test.appPaterno ||
+      !this.test.appMaterno ||
+      !this.test.nombre ||
+      !this.test.programaInteres ||
+      (this.test.programaInteres === 'other' && !this.test.otroPrograma) ||
+      !this.test.bachilleratoProcedencia ||
+      (this.test.bachilleratoProcedencia === 'other' &&
+        !this.test.otroBachillerato) ||
+      !this.test.telefono ||
+      !this.test.email ||
+      !this.test.facebook
+    ) {
+      alert('Por favor, completa todos los campos obligatorios.');
+      return;
+    }
+
+    this.submitted = true;
     this.testUvWebService.create(this.test).then(() => {
       console.log('Created new item successfully!');
       this.submitted = true;
